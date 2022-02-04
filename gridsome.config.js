1f82d53dcdc24ffcda4ae411846646af85631303
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2022-02-04 22:59:24 +0700

    build: update deps

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,37 +1,39 @@
-// This is where project configuration and plugin options are located. 
+// This is where project configuration and plugin options are located.
 // Learn more: https://gridsome.org/docs/config
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Nathan Yiangsupapaanontr',
-  siteUrl: 'https://zartre.com',
+  siteName: "Nathan Yiangsupapaanontr",
+  siteUrl: "https://zartre.com",
   plugins: [
     {
-      use: '@gridsome/source-filesystem',
+      use: "@gridsome/source-filesystem",
       options: {
-        path: 'content/work/**/*.md',
-        typeName: 'Work',
+        path: "content/work/**/*.md",
+        typeName: "Work",
         refs: {
           tags: {
-            typeName: 'Tag',
-            create: true
-          }
-        }
-      }
+            typeName: "Tag",
+            create: true,
+          },
+        },
+      },
     },
     {
-      use: '@gridsome/plugin-google-analytics',
+      use: "@gridsome/plugin-google-analytics",
       options: {
-        id: 'G-B064NPS19C'
-      }
-    }
+        id: "G-B064NPS19C",
+      },
+    },
   ],
   templates: {
-    Work: [{
-      name: 'work',
-      path: '/work/:slug'
-    }]
-  }
+    Work: [
+      {
+        name: "work",
+        path: "/work/:slug",
+      },
+    ],
+  },
 };
